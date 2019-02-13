a6fd5779c0dbc8a1d611c736d2e3a83dd61f5d0f
Ahmed El Atari <elatariahmed@gmail.com>
2019-02-13 18:15:01 +0100

    ghhgf
    ghjghj

--- src/App.js
+++ src/App.js
@@ -19,7 +19,7 @@ import { Collapse } from "antd";
 const Panel = Collapse.Panel;
 
 const text = `
-  A dog is a type of domesticated animal.
+  A dog is a type of  person.
   Known for its loyalty and faithfulness,
   it can be found as a welcome guest in many households across the world.
 `;
